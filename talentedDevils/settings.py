74d444bba2445d38927c41d7bcc07c58b1d6c477
Aziz Abdullaev <aziz05072000@outlook.com>
2022-05-22 18:17:06 -0400

    Update settings.py

--- talentedDevils/settings.py
+++ talentedDevils/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "*hL2YQzUcc72GqdWnR8Xiep%nBjVtFgzZ&Ea&CQNV4TojNjFHtRAZrBAcy$W&b"
+SECRET_KEY = "YOUR_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
